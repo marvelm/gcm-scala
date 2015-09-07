3959e0179c3791c35bed3b222e10958d865d5a79
m <m@marvelmathew.ca>
2015-09-06 21:39:53 -0500

    Forward Smack connection events to listener

--- src/main/scala/gcm/SmackGcm.scala
+++ src/main/scala/gcm/SmackGcm.scala
@@ -20,23 +20,37 @@ class SmackGcm(config: GcmConfig) {
     .build()
   val conn = new XMPPTCPConnection(smackConf)
 
+  val listener = config.listener
+
   conn.addConnectionListener(new ConnectionListener {
     override def connected(connection: XMPPConnection): Unit = {
-      println("Connected")
       conn.login()
+      listener ! ("Connected", connection)
     }
 
-    override def reconnectionFailed(e: Exception): Unit = e.printStackTrace()
+    override def reconnectionFailed(e: Exception): Unit ={
+      listener ! ("ReconnectionFailed", e)
+    }
 
-    override def reconnectionSuccessful(): Unit = println("Reconnected")
+    override def reconnectionSuccessful(): Unit ={
+      listener ! "ReconnectionSuccessful"
+    }
 
-    override def authenticated(connection: XMPPConnection, resumed: Boolean): Unit = println("Authenticated")
+    override def authenticated(connection: XMPPConnection, resumed: Boolean): Unit = {
+      listener ! ("Authenticated", connection, resumed)
+    }
 
-    override def connectionClosedOnError(e: Exception): Unit = e.printStackTrace()
+    override def connectionClosedOnError(e: Exception): Unit = {
+      listener ! ("ConnectionClosedOnError", e)
+    }
 
-    override def connectionClosed(): Unit = println("Connection closed")
+    override def connectionClosed(): Unit = {
+      listener ! "ConnectionClosed"
+    }
 
-    override def reconnectingIn(seconds: Int): Unit = println(seconds)
+    override def reconnectingIn(seconds: Int): Unit = {
+      listener ! ("ReconnectingIn", seconds)
+    }
   })
 
   conn.addAsyncStanzaListener(new StanzaListener {
